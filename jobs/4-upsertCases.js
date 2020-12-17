1c01f9a18d9c048ede53df503e9bf839e78ad319
Taylor Downs <downs.taylor@gmail.com>
2020-12-17 07:31:13 -0500

    initial pattern for upsert

--- jobs/4-upsertCases.js
+++ jobs/4-upsertCases.js
@@ -1,1 +1,18 @@
-//Upsert cases from GoData based on Case Id
+// Upsert cases from GoData based on Case Id
+
+upsertCase(
+  'd4eecdeb-252d-44ec-bf76-515e005e03bd', // the outbreak ID
+  {
+    externalId: 'visual_id',
+    data: {
+      firstName: dataValue('body.Patient_name').split(' ')[0],
+      lastName: dataValue('body.Patient_name').split(' ')[1],
+      classification: dataValue('body.Sample_Classification'),
+      visual_id: dataValue('body.Case_ID'),
+      'age:years': dataValue('body.Age_in_year'),
+      'addresses:country': 'Bangladesh',
+      'addresses:city': dataValue('patient_address/Upazilla'),
+      gender: dataValue('body.Sex'),
+    },
+  }
+);
