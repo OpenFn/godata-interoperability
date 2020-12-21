d958fb7e9a454d956395f8865d917e5c4ff8e404
mamadou <mamadou.lakhassane@gmail.com>
2020-12-21 14:11:30 +0000

    Fetch cases from Go.Data and bulk post to inbox, #5

--- jobs/getCases.js
+++ jobs/getCases.js
@@ -1,1 +1,39 @@
 //Job to fetch Cases to sync to other system
+
+// Fetch cases from Go.Data matching a specific outbreak id
+listCases('3b5554d7-2c19-41d0-b9af-475ad25a382b', {}, state => {
+  state.cases = state.data.map(element => {
+    return {
+      name: `${element.firstName}, ${element.lastName || ''}`,
+      status: element.classification,
+      externalId: element.id,
+      caseId: element.visualId,
+      age: element.age ? element.age.years : element['age:years'],
+      phone:
+        element.addresses && element.addresses[0]
+          ? element.addresses[0].phoneNumber
+          : element['addresses:phoneNumber'],
+      country:
+        element.addresses && element.addresses[0]
+          ? element.addresses[0].country
+          : element['addresses:country'],
+      location:
+        element.addresses && element.addresses[0]
+          ? element.addresses[0].locationId
+          : element['addresses:locationId'],
+    };
+  });
+  return state;
+});
+
+// Bulk post to OpenFn Inbox
+alterState(state => {
+  const { openfnInboxUrl, username, password_post } = state.configuration;
+  const data = state.cases;
+  console.log(`Sending to OpenFn Inbox in bulk...`);
+  return post(`${openfnInboxUrl}`, {
+    body: { data },
+    headers: { 'content-type': 'json' },
+    authentication: { username, password: password_post },
+  })(state);
+});
