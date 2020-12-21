387aabf66037ee5e819f7b8c1d33f4bf01efc1ca
mamadou <mamadou.lakhassane@gmail.com>
2020-12-21 12:34:36 +0000

    using name as externalId, #2

--- jobs/5-uploadHealthSites.js
+++ jobs/5-uploadHealthSites.js
@@ -1,33 +1,36 @@
 //Job to upload Health Sites to Go.Data as reference data
 alterState(state => {
-  let amenities = [];
+  let locations = [];
   Object.keys(state.data).forEach(key => {
     if (state.data[key].attributes) {
       const { amenity } = state.data[key].attributes;
-      if (amenity === 'hospital') amenities.push(state.data[key]);
+      if (amenity === 'hospital') locations.push(state.data[key]);
     }
   });
 
-  return { ...state, amenities };
+  const valid_locations = locations.filter(
+    location => location.attributes.name !== undefined
+  );
+  return { ...state, locations: valid_locations };
 });
 
 each(
-  '$.amenities[*]',
+  '$.locations[*]',
   alterState(state => {
     const { attributes, centroid } = state.data;
     const coordinates = centroid.coordinates;
     const data = {
       name: attributes.name,
-      synonyms: [attributes.amenity],
+      // synonyms: [attributes.amenity],
       identifiers: [{ description: 'uuid', code: attributes.uuid }],
-      geoLocation: { lat: coordinates[0], lng: coordinates[1] },
+      geoLocation: { lat: coordinates[1], lng: coordinates[0] },
       active: true,
       parentLocationId: 'e86414e4-d91c-4ab8-be2a-720ae90b5106',
       geographicalLevelId:
         'LNG_REFERENCE_DATA_CATEGORY_LOCATION_GEOGRAPHICAL_LEVEL_HOSPITAL_FACILITY',
     };
-    return upsertLocation({
-      externalId: `${attributes.uuid}`,
+
+    return upsertLocation('name', {
       data,
     })(state);
   })
