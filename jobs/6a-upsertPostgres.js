62842878c8441ac8b14a69c1d97309718d654667
mamadou <mamadou.lakhassane@gmail.com>
2020-12-22 16:32:46 +0000

    adding logs for postgres db, #5

--- jobs/6a-upsertPostgres.js
+++ jobs/6a-upsertPostgres.js
@@ -1,14 +1,14 @@
 // Your job goes here.
 alterState(state => {
+  console.log('Upserting in HMIS database...');
+  console.log(state.cases);
 
-    return upsertMany('tbl_cases', 'ON CONSTRAINT tbl_cases_pkey', state =>
-      state.cases.map(element => {
-        return {
-          // Here goes the mapping
-          // attribute: element.attribute
-        };
-      })
-    )(state);
-  });
-  
-  
+  return upsertMany('tbl_cases', 'ON CONSTRAINT tbl_cases_pkey', state =>
+    state.cases.map(report => {
+      return {
+        // Here goes the mapping
+        // attribute: element.attribute
+      };
+    })
+  )(state);
+});
