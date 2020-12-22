23c0679a03371a7b656d32fad0bf34c8109cd351
Aleksa Krolls <aleksagk@gmail.com>
2020-12-22 08:36:39 +0200

    aleksa@openfn.org's change on OpenFn.org

--- jobs/5a-GETHealthSitesData.js
+++ jobs/5a-GETHealthSitesData.js
@@ -1,34 +1,3 @@
-/** 
- * Example HealthSite Endpoints
- *     
- * 
-    {
-      "path": "/api/schema/",
-      "description": "Schema list"
-    },
-    {
-      "path": "/api/v2/facilities/",
-      "description": "Returns a list of facilities with some filtering parameters."
-    },
-    {
-      "path": "/api/v2/facilities/way/454875140",
-      "description": "Returns a facility detail."
-    },
-    {
-      "path": "/api/v2/facilities/by-uuid/08e91a3157734f7dbc4ccceade9294be",
-      "description": "Get facility by uuid"
-    },
-    {
-      "path": "/api/v2/facilities/count",
-      "description": "Returns count of facilities with some filtering parameters."
-    },
-    {
-      "path": "/api/v2/facilities/statistic",
-      "description": "Returns statistic of facilities with some filtering parameters."
-    }
-
- */
-
 alterState(state => {
   // Replace this endpoint with any of the examples above
   let endpoint = {
@@ -81,3 +50,35 @@ post(
     return state;
   }
 );
+
+/** 
+ * Example HealthSite Endpoints
+ *     
+ * 
+    {
+      "path": "/api/schema/",
+      "description": "Schema list"
+    },
+    {
+      "path": "/api/v2/facilities/",
+      "description": "Returns a list of facilities with some filtering parameters."
+    },
+    {
+      "path": "/api/v2/facilities/way/454875140",
+      "description": "Returns a facility detail."
+    },
+    {
+      "path": "/api/v2/facilities/by-uuid/08e91a3157734f7dbc4ccceade9294be",
+      "description": "Get facility by uuid"
+    },
+    {
+      "path": "/api/v2/facilities/count",
+      "description": "Returns count of facilities with some filtering parameters."
+    },
+    {
+      "path": "/api/v2/facilities/statistic",
+      "description": "Returns statistic of facilities with some filtering parameters."
+    }
+
+ */
+
