d90fd5c9e82afb60a4a1cc37f58e83f088354c1a
mamadou <mamadou.lakhassane@gmail.com>
2020-12-22 10:41:53 +0000

    renaming jobs

---
+++ jobs/6a-upsertPostgres.js
@@ -0,0 +1,12 @@
+alterState(state => {
+
+  return upsertMany('tbl_cases', 'ON CONSTRAINT tbl_cases_pkey', state =>
+    state.cases.map(element => {
+      return {
+        // Here goes the mapping
+        // attribute: element.attribute
+      };
+    })
+  )(state);
+});
+
