5f2e7c9bcc098150b6ab8e2f7d97519f29313fad
mamadou <mamadou.lakhassane@gmail.com>
2020-12-22 12:16:57 +0000

    upsert postgres table

--- jobs/6a-upsertPostgres.js
+++ jobs/6a-upsertPostgres.js
@@ -1,1 +1,14 @@
 // Your job goes here.
+alterState(state => {
+
+    return upsertMany('tbl_cases', 'ON CONSTRAINT tbl_cases_pkey', state =>
+      state.cases.map(element => {
+        return {
+          // Here goes the mapping
+          // attribute: element.attribute
+        };
+      })
+    )(state);
+  });
+  
+  
