3ef7ae31fa68c41dfd11019ab5ed2b457736bb08
mamadou <mamadou.lakhassane@gmail.com>
2021-01-05 12:04:57 +0000

    checking if summary is undefined to avoid failing job, #8

--- jobs/6b-importDHIS2.js
+++ jobs/6b-importDHIS2.js
@@ -1,27 +1,35 @@
 alterState(state => {
-  const { dateOfReporting, value } = state.summary;
+  if (state.summary !== undefined) {
+    const { dateOfReporting, value } = state.summary;
 
-  const dataValue = {
-    completeDate: `${dateOfReporting.substring(
-      0,
-      4
-    )}-${dateOfReporting.substring(5, 7)}-${dateOfReporting.substring(8, 10)}`,
-    dataSet: 'kIfMNugiTgd',
-    period: `${dateOfReporting.substring(0, 4)}${dateOfReporting.substring(
-      5,
-      7
-    )}${dateOfReporting.substring(8, 10)}`,
-    orgUnit: 'CnPsS2xE8UN',
-    dataValues: [
-      {
-        dataElement: 'CnPsS2xE8UN',
-        value,
-      },
-    ],
-  };
+    const dataValue = {
+      completeDate: `${dateOfReporting.substring(
+        0,
+        4
+      )}-${dateOfReporting.substring(5, 7)}-${dateOfReporting.substring(
+        8,
+        10
+      )}`,
+      dataSet: 'kIfMNugiTgd',
+      period: `${dateOfReporting.substring(0, 4)}${dateOfReporting.substring(
+        5,
+        7
+      )}${dateOfReporting.substring(8, 10)}`,
+      orgUnit: 'CnPsS2xE8UN',
+      dataValues: [
+        {
+          dataElement: 'CnPsS2xE8UN',
+          value,
+        },
+      ],
+    };
 
-  console.log('Importing data on DHIS2...');
-  console.log(dataValue);
+    console.log('Importing data on DHIS2...');
+    console.log(dataValue);
 
-  return dataValueSet(dataValue)(state);
+    return dataValueSet(dataValue)(state);
+  } else {
+    console.log('No cases were imported to DHIS2...');
+    return state;
+  }
 });
